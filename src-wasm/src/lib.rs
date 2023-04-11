53f336076526136c28313c276aa12c57710d59e1
eaon <eaon@posteo.net>
2023-04-11 09:28:23 -0400

    Use JS Number in WASM rather than BigInt in JS

--- src-wasm/src/lib.rs
+++ src-wasm/src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
+use js_sys::Number;
 
 #[wasm_bindgen]
-pub fn decrement(current: i64) -> i64 {
-    current - 1
+pub fn decrement(current: Number) -> Number {
+    (current.value_of() - 1f64).into()
 }
